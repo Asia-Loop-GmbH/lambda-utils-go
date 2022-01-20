9ffa6b85a4be9928c9c8b511c5cf3b6950529116
Nam Trường Lê <lenamtruong@gmail.com>
2022-01-20 08:06:26 +0100

    success http response

--- rest.go
+++ rest.go
@@ -6,6 +6,21 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 )
 
+func HTTPResponse(status int, request *events.APIGatewayProxyRequest, body interface{}) *events.APIGatewayProxyResponse {
+	bodyString, err := json.Marshal(body)
+	if err != nil {
+		return HTTPErrorResponse(500, request, err)
+	}
+	return &events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       string(bodyString),
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Headers": "*",
+		},
+	}
+}
+
 func HTTPErrorResponse(status int, request *events.APIGatewayProxyRequest, err error) *events.APIGatewayProxyResponse {
 	body := admin.HttpErrorBody{
 		Message:     err.Error(),
