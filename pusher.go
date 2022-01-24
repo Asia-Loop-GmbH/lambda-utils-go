b0cc01222d9ec98fbec61e6f12fbb3ec44e23c42
Nam Trường Lê <lenamtruong@gmail.com>
2022-01-24 06:46:38 +0100

    fix pusher client

--- pusher.go
+++ pusher.go
@@ -3,15 +3,15 @@ package lambda_utils_go
 import "github.com/pusher/pusher-http-go/v5"
 
 func NewPusherClient(stage *string) (*pusher.Client, error) {
-	app, err := GetSSMParameter(*stage, "/dev/pusher/app", false)
+	app, err := GetSSMParameter(*stage, "/pusher/app", false)
 	if err != nil {
 		return nil, err
 	}
-	key, err := GetSSMParameter(*stage, "/dev/pusher/key", false)
+	key, err := GetSSMParameter(*stage, "/pusher/key", false)
 	if err != nil {
 		return nil, err
 	}
-	secret, err := GetSSMParameter(*stage, "/dev/pusher/secret", true)
+	secret, err := GetSSMParameter(*stage, "/pusher/secret", true)
 	if err != nil {
 		return nil, err
 	}
