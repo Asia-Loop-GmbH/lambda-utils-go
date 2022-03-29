b6ce599bab293e5bf7ef85bad026e0d3579ebcba
Nam Trường Lê <lenamtruong@gmail.com>
2022-03-29 14:31:49 +0200

    add ticket status

--- pkg/dbadmin/ticket.go
+++ pkg/dbadmin/ticket.go
@@ -6,6 +6,15 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type TicketStatus string
+
+const (
+	TicketStatusNew        TicketStatus = "NEW"
+	TicketStatusClosed     TicketStatus = "CLOSED"
+	TicketStatusInProgress TicketStatus = "IN_PROGRESS" // TicketStatusInProgress admin must process it
+	TicketStatusPending    TicketStatus = "PENDING"     // TicketStatusPending service worker must process it
+)
+
 type TicketCustomer struct {
 	FirstName      string `bson:"firstName" json:"firstName"`
 	LastName       string `bson:"lastName" json:"lastName"`
@@ -24,6 +33,7 @@ type TicketMessage struct {
 type Ticket struct {
 	ID           primitive.ObjectID `bson:"_id" json:"id"`
 	TicketNumber string             `bson:"ticketNumber" json:"ticketNumber"`
+	Status       TicketStatus       `bson:"status" json:"status"`
 	StoreKey     string             `bson:"storeKey" json:"storeKey"`
 	Customer     TicketCustomer     `bson:"customer" json:"customer"`
 	CreatedAt    time.Time          `bson:"createdAt" json:"createdAt"`
