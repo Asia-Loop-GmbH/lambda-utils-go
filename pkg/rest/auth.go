7944e6c49f002147e1cc03a67d58413b838af04d
Nam Trường Lê <lenamtruong@gmail.com>
2022-04-19 09:25:26 +0200

    add rest.AuthorizeAnalyticsRequest

---
+++ pkg/rest/auth.go
@@ -0,0 +1,31 @@
+package rest
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+
+	"github.com/asia-loop-gmbh/lambda-utils-go/v3/pkg/servicessm"
+)
+
+func AuthorizeAnalyticsRequest(log *logrus.Entry, ctx context.Context, request *events.APIGatewayProxyRequest) error {
+	log.Infof("authorize analytics request [%s] [%s]", request.HTTPMethod, request.Path)
+
+	keyInReq, hasKey := request.Headers["X-API-Key"]
+	if !hasKey {
+		return errors.New("missing API key")
+	}
+
+	key, err := servicessm.GetParameter(log, ctx, "all", "/analytics/key", true)
+	if err != nil {
+		return err
+	}
+
+	if keyInReq != *key {
+		return errors.New("unauthorized")
+	}
+
+	return nil
+}
